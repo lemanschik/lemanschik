5d043ce38c06478985e870f9482189aca28c5637
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-07-07 08:08:22 +0200

    Update bfcache.js

--- bfcache/bfcache.js
+++ bfcache/bfcache.js
@@ -4,4 +4,27 @@ window.onpagehide = (event) => {
   } else {
     console.log('This page will unload normally and be discarded.');
   }
+};
+
+window.onpageshow = (event) => {
+  if (event.persisted) {
+    console.log('This page was restored from the bfcache.');
+  } else {
+    console.log('This page was loaded normally.');
+  }
+};
+
+function beforeUnloadListener(event) {
+  event.preventDefault();
+  return event.returnValue = 'Are you sure you want to exit?';
+};
+
+// A function that invokes a callback when the page has unsaved changes.
+onPageHasUnsavedChanges(() => {
+  window.onbeforeunload = beforeUnloadListener;
+});
+
+// A function that invokes a callback when the page's unsaved changes are resolved.
+onAllChangesSaved(() => {
+  window.onbeforeunload = beforeUnloadListener;
 });
