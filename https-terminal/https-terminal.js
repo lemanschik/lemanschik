fc0cfe41d282b686bfa808a8d837ffbf092210a4
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-12-25 13:08:35 +0100

    Update https-terminal.js

--- https-terminal/https-terminal.js
+++ https-terminal/https-terminal.js
@@ -14,3 +14,26 @@ globalThis.addEventListener('fetch', (event) => {
   ))
 });
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((registration) => {
+      registration.addEventListener("updatefound", () => {
+        // If updatefound is fired, it means that there's
+        // a new service worker being installed.
+        const installingWorker = registration.installing;
+        console.log(
+          "A new service worker is being installed:",
+          installingWorker,
+        );
+
+        // You can listen for changes to the installing service worker's
+        return new Promise(resolve=>(installingWorker.onstatechange = () => resolve(installingWorker));
+      });
+    })
+    .catch((error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    });
+} else {
+  console.error("Service workers are not supported.");
+}
